15257feee222007ce04085adb230d9911a234815
Ana Elisa G Zanon <116923556+anaelisazanon@users.noreply.github.com>
2022-11-16 21:54:21 -0300

    Update A18_280922_While_LerNMaiorMenor.cpp

--- A18_280922_While_LerNMaiorMenor.cpp
+++ A18_280922_While_LerNMaiorMenor.cpp
@@ -1,5 +1,6 @@
-//Atividade 
+//Atividade 11
 //Usando laço de repetição, faça um programa que leia N números e imprima o maior e o menor deles
+
 
 #include <iostream>
 using namespace std;
@@ -8,19 +9,28 @@ int main()
 {
     int cont = 1;
     int total_de_numeros;
-    int numero_maior = 0;
-    int numero_menor = 1000; //para sempre ser menor
+    int numero_maior;
+    int numero_menor; 
     int numero; //variavel para fazer comparação; //n1, n2, n3
     
     
     cout << "Digite a total de números: ";
     cin >> total_de_numeros;
     
+    //laço especial só para adicionar um valor para o maior e para o menor numero 
+    //  dessa forma não adc na declaracao de variavel um numero pequeno para maior e grande para menor
+    cout << "\n Digite o " << cont << "o numero: ";
+    cin >> numero;
+        
     numero_menor = numero;
-    
+    numero_maior = numero;
+       
+    cont++;  
+ 
+    //laço
     while (cont <= total_de_numeros){
         
-        cout << "\n Digite o " << cont << "o numero: ";
+        cout << "\n Digite o " << cont << " numero: ";
         cin >> numero;
         
         cont++;
