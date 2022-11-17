ac3e804b919297044540f4aa1c7b130c1e3fc0fe
Ana Elisa G Zanon <116923556+anaelisazanon@users.noreply.github.com>
2022-11-16 21:36:58 -0300

    Create A07_310822_NumeroMaiorMenor.cpp

---
+++ A07_310822_NumeroMaiorMenor.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+1) Faça um programa que leia três números inteiros e imprima: o menor, o maior e o intermediário.
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1 = 3;
+    int n2 = 5;
+    
+    if(n1 > n2)
+    {
+        cout << "\nN1 é maior que N2";
+    }   
+    else{
+    cout << "\nN2 é maior que N1";
+    }
+   
+    
+    return 0;
+}
