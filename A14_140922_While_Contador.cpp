d1802a3ac810e27367f803390b3ef9ceca63b57b
Ana Elisa G Zanon <116923556+anaelisazanon@users.noreply.github.com>
2022-11-16 21:44:58 -0300

    Create A14_140922_While_Contador.cpp

---
+++ A14_140922_While_Contador.cpp
@@ -0,0 +1,17 @@
+//contador
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int cont = 0;
+    
+    while(cont < 10){
+        
+        cont++;
+        cout << "\ncont " << cont;
+    }
+
+    return 0;
+}
