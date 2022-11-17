8e3f68757a29a3eb251bfbb628a3dad6f067c579
Ana Elisa G Zanon <116923556+anaelisazanon@users.noreply.github.com>
2022-11-16 22:01:08 -0300

    Update A22_280922_SwitchCase_Calculadora.cpp

--- A22_280922_SwitchCase_Calculadora.cpp
+++ A22_280922_SwitchCase_Calculadora.cpp
@@ -1,51 +1,57 @@
 //calculadora
 //digitar n1, operador, n2, resultado
+//pergunta se quer continuar
 
 #include <iostream>
 using namespace std;
 
 int main(){
     
-    int n1, n2, resultado, numero;
+    int n1, n2;
+    float resultado;
+    char continua = 's';
     char op;  // = 'c'   unica letra e aspas simples
-    
-    cout << "Digite um numero: ";
-    cin >> n1;
-    
-    cout << "Digite outro numero: ";
-    cin >> n2;
 
-
-    cout << "Digite o operador: ";
-    cin >> op;
-    
-    switch (op){
-        case '+': 
-            resultado = n1 + n2;
-            cout << "Resultado é " << resultado;
-            break;
-            
-        case '-': 
-            resultado = n1 - n2;
-            cout << "Resultado é " << resultado;
-            break;
-            
-        case '/': 
-            resultado = n1 / n2;
-            cout << "Resultado é " << resultado;
-            break;      
-            
-        case '*': 
-            resultado = n1 * n2;
-            cout << "Resultado é " << resultado;
-            break;
- 
-        default:
-            cout << "Não é válido";
-            break;
+    while ((continua == 's') || (continua == 'S')) {
+        cout << "Digite um numero: ";
+        cin >> n1;
         
+        cout << "Digite outro numero: ";
+        cin >> n2;
+    
+        cout << "Digite o operador: ";
+        cin >> op;
+        
+        switch (op){
+            case '+': 
+                resultado = n1 + n2;
+                cout << "Resultado é " << resultado;
+                break;
+                
+            case '-': 
+                resultado = n1 - n2;
+                cout << "Resultado é " << resultado;
+                break;
+                
+            case '/': 
+                resultado = n1 / n2;
+                cout << "Resultado é " << resultado;
+                break;      
+                
+            case '*': 
+                resultado = n1 * n2;
+                cout << "Resultado é " << resultado;
+                break;
+     
+            default:
+                cout << "Não é válido";
+                break;
+        
+        
+        }
+        cout << "\nDeseja fazer mais uma operação? Digite s Ou n";
+        cin >> continua;
     }
 
-    
     return 0;
 }
