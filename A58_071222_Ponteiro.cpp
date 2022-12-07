c33018161ee82e35d642968a0befdd9df17b21a7
Ana Elisa G Zanon <116923556+anaelisazanon@users.noreply.github.com>
2022-12-07 16:03:23 -0300

    Update A58_071222_Ponteiro.cpp

--- A58_071222_Ponteiro.cpp
+++ A58_071222_Ponteiro.cpp
@@ -9,17 +9,30 @@ int main()
     int var = 45;
     int *p1;
     int *p2;
+    int vet[0];
     
-    
-    p = &var; // p recebe conteúdo (endereço do VAR) //45 n importa
+    p1 = &var; // p recebe conteúdo (endereço do VAR) //45 n importa
     
     cout << var;
 
-    cout << "\n" << &var; //diz o conteúdo endereço do var
-    cout << "\n" << p1; //diz o conteúdo endereço de var
-    cout << "\n" << &p1; // diz o endereço do p
-    cout << "\n" << *p1; //diz o conteúdo do var pq o p1 aponta pro var
+    cout << "\n var  " << &var; //diz o conteúdo endereço do var
+    cout << "\n&var  " << p1; //diz o conteúdo endereço de var
+    cout << "\n&p1  " << &p1; // diz o endereço do p
+    cout << "\n*p1  " << *p1; //diz o conteúdo do var pq o p1 aponta pro var
+    
+    p2 = p1;
+    
+    cout << "\np2  " << p2;
+    
+    cout << "\n*p2  " << *p2;
+    
+    *p2 = 40;
+    cout << "\n*p2  " << *p2;
+    cout << "\n*var  " << var; // n acessou a variável mas trocou o valor pelo *p2
     
     
+    p1 = vet; // p1 = &vet[0];  endereco do vetor é o da primeira posicao
+    cout << "\np1  " << p1;
+
     return 0;
 }
